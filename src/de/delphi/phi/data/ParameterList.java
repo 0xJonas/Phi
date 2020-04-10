f6438ef112db7fa06cee975b9e61516f9d8abde9
Jonas Rinke <j.rinke@htp-tel.de>
2020-04-10 17:18:11 +0200

    Added javadoc to ParameterList

--- src/de/delphi/phi/data/ParameterList.java
+++ src/de/delphi/phi/data/ParameterList.java
@@ -3,15 +3,29 @@ package de.delphi.phi.data;
 import de.delphi.phi.PhiArgumentException;
 import de.delphi.phi.PhiRuntimeException;
 import de.delphi.phi.PhiScope;
+import de.delphi.phi.parser.PhiInternalException;
 
 import java.util.List;
 
+/**
+ * Represents the list of parameters that a PhiFunction accepts.
+ */
 public class ParameterList {
 
+    /**
+     * Names of the parameters
+     */
     private String[] names;
 
+    /**
+     * Default values of the parameters, if provided
+     */
     private PhiScope defaultValues;
 
+    /**
+     * stores which names have default values associated with them. Used to check if a function call has a value for
+     * every parameter.
+     */
     private boolean[] availableValues;
 
     public ParameterList(List<String> names, PhiScope defaultValues){
@@ -24,16 +38,22 @@ public class ParameterList {
         }
     }
 
+    /**
+     * Returns a copy of the default values. This can be used as the scope of the function call.
+     */
     public PhiScope getDefaultValues(){
         try {
             return (PhiScope) defaultValues.clone();
         }catch(CloneNotSupportedException e){
-            e.printStackTrace();
-            return null;    //should never happen
+            throw new PhiInternalException(e);
         }
     }
 
-    public int indexOf(String name){
+    /**
+     * Returns the index of the given name in the names array, or -1 if it is not found.
+     * @param name The name to get the index for.
+     */
+    private int indexOf(String name){
         for(int i = 0; i < names.length; i++){
             if(names[i].equals(name))
                 return i;
@@ -41,6 +61,17 @@ public class ParameterList {
         return -1;
     }
 
+    /**
+     * Creates a scope from the names in this ParameterList and the values given as an argument.
+     *
+     * The scope is set up in the following way: Firstly the unnamed members of the collection are put into the
+     * scope in order. Secondly the named members of the collection are added to the scope. This may override values
+     * already set with unnamed parameters.
+     *
+     * @param params A collection containing values for parameters.
+     * @return A PhiScope in which the parameter names are associated with the given values.
+     * @throws PhiRuntimeException If too few, too many or wrong parameters are supplied.
+     */
     public PhiScope supplyParameters(PhiCollection params) throws PhiRuntimeException {
         if(params.getLength().longValue() > names.length)
             throw new PhiArgumentException("Too many parameters.");
