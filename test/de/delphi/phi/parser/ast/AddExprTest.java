75b22d3bf718de62660fe957c87162c79077ddd9
Jonas Rinke <j.rinke@htp-tel.de>
2020-04-15 11:30:43 +0200

    Rewrote AddExprTest to use parser

--- test/de/delphi/phi/parser/ast/AddExprTest.java
+++ test/de/delphi/phi/parser/ast/AddExprTest.java
@@ -1,12 +1,17 @@
 package de.delphi.phi.parser.ast;
 
+import de.delphi.phi.PhiException;
 import de.delphi.phi.PhiRuntimeException;
 import de.delphi.phi.PhiScope;
 import de.delphi.phi.PhiTypeException;
-import de.delphi.phi.data.*;
+import de.delphi.phi.data.PhiInt;
+import de.delphi.phi.data.PhiObject;
+import de.delphi.phi.data.PhiSymbol;
+import de.delphi.phi.data.Type;
+import de.delphi.phi.parser.Parser;
 import org.junit.Test;
 
-import java.util.List;
+import java.io.IOException;
 
 import static de.delphi.phi.Polyfill.assertThrows;
 import static org.junit.Assert.assertEquals;
@@ -16,97 +21,64 @@ public class AddExprTest {
     private PhiScope createScope() throws PhiRuntimeException {
         PhiScope scope = new PhiScope();
         scope.createMember(new PhiSymbol("a"));
-        scope.createMember(new PhiSymbol(".a"));
-        scope.createMember(new PhiSymbol(".b"));
+        scope.createMember(new PhiSymbol("qa"));
+        scope.createMember(new PhiSymbol("qb"));
         scope.setNamed("a", new PhiInt(5));
-        scope.setNamed(".a", new PhiSymbol("a"));
-        scope.setNamed(".b", new PhiSymbol("b"));
+        scope.setNamed("qa", new PhiSymbol("a"));
+        scope.setNamed("qb", new PhiSymbol("b"));
         return scope;
     }
 
+    private PhiObject parseAndEval(String input, PhiScope scope) throws PhiException, IOException{
+        Parser parser = new Parser(input);
+        Expression expr = parser.nextExpression();
+        return expr.eval(scope);
+    }
+
     @Test
-    public void testIntAdd() throws PhiRuntimeException{
-        AddExpr expr = new AddExpr(
-                List.of(
-                        new Atom(new PhiSymbol("a")),
-                        new Atom(new PhiInt(3)),
-                        new Atom(new PhiInt(2))),
-                List.of(AddExpr.OP_ADD, AddExpr.OP_ADD, AddExpr.OP_SUB));
+    public void testIntAdd() throws PhiException, IOException{
         PhiScope scope = createScope();
-        PhiObject result = expr.eval(scope);
+        PhiObject result = parseAndEval("a + 3 - 2", scope);
         assertEquals(Type.INT, result.getType());
         assertEquals(6, result.longValue());
     }
 
     @Test
-    public void testFloatAdd() throws PhiRuntimeException{
-        AddExpr expr = new AddExpr(
-                List.of(
-                        new Atom(new PhiSymbol("a")),
-                        new Atom(new PhiFloat(2.5)),
-                        new Atom(new PhiInt(2))),
-                List.of(AddExpr.OP_ADD, AddExpr.OP_ADD, AddExpr.OP_SUB));
+    public void testFloatAdd() throws PhiException, IOException{
         PhiScope scope = createScope();
-        PhiObject result = expr.eval(scope);
+        PhiObject result = parseAndEval("a + 2.5 - 2", scope);
         assertEquals(Type.FLOAT, result.getType());
         assertEquals(5.5, result.doubleValue(), 1e-9);
     }
 
     @Test
-    public void testStringConcat() throws PhiRuntimeException{
-        AddExpr expr1 = new AddExpr(
-                List.of(
-                        new Atom(new PhiString("a")),
-                        new Atom(new PhiString("b"))),
-                List.of(AddExpr.OP_ADD, AddExpr.OP_ADD));
+    public void testStringConcat() throws PhiException, IOException{
         PhiScope scope = createScope();
-        PhiObject result = expr1.eval(scope);
+        PhiObject result = parseAndEval("\"a\" + \"b\"", scope);
         assertEquals(Type.STRING, result.getType());
         assertEquals("ab", result.toString());
 
-        AddExpr expr2 = new AddExpr(
-                List.of(
-                        new Atom(new PhiString("a")),
-                        new Atom(new PhiString("b"))),
-                List.of(AddExpr.OP_ADD, AddExpr.OP_SUB));
         assertThrows("Subtracting strings succeeded", PhiTypeException.class,
-                ()->expr2.eval(scope)
+                ()->parseAndEval("\"a\" - \"b\"", scope)
         );
     }
 
     @Test
-    public void testSymbolConcat() throws PhiRuntimeException{
-        AddExpr expr1 = new AddExpr(
-                List.of(
-                        new Atom(new PhiSymbol(".a")),
-                        new Atom(new PhiSymbol(".b"))),
-                List.of(AddExpr.OP_ADD, AddExpr.OP_ADD));
+    public void testSymbolConcat() throws PhiException, IOException{
         PhiScope scope = createScope();
-        PhiObject result = expr1.eval(scope);
+        PhiObject result = parseAndEval("qa + qb", scope);
         assertEquals(Type.SYMBOL, result.getType());
         assertEquals("ab", result.toString());
 
-        AddExpr expr2 = new AddExpr(
-                List.of(
-                        new Atom(new PhiSymbol(".a")),
-                        new Atom(new PhiSymbol(".b"))),
-                List.of(AddExpr.OP_ADD, AddExpr.OP_SUB));
         assertThrows("Subtracting symbols succeeded", PhiTypeException.class,
-                ()->expr2.eval(scope)
+                ()->parseAndEval("qa - qb", scope)
         );
     }
 
     @Test
-    public void testReturnThrough() throws PhiRuntimeException{
-        WhileExpr expr = new WhileExpr(new Atom(PhiInt.TRUE),
-                new AddExpr(
-                        List.of(
-                                new BreakExpr(
-                                        new Atom(new PhiInt(1))),
-                                new Atom(new PhiInt(2))),
-                        List.of(AddExpr.OP_ADD, AddExpr.OP_ADD)));
+    public void testReturnThrough() throws PhiException, IOException{
         PhiScope scope = createScope();
-        PhiObject result = expr.eval(scope);
+        PhiObject result = parseAndEval("while true do ((break(1)) + 2)", scope);
         assertEquals(1, result.longValue());
     }
 }
